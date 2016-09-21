885140e7d6426345d8a347c511bc52a67799c49d
Chris Jackson <chris@cd-jackson.com>
2016-09-21 22:01:50 +0200

    Set battery node to DEAD if no response within 2x wakeup period (#119)
    fixes #55
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>
    
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- src/main/java/org/openhab/binding/zwave/handler/ZWaveThingHandler.java
+++ src/main/java/org/openhab/binding/zwave/handler/ZWaveThingHandler.java
@@ -51,6 +51,7 @@ import org.openhab.binding.zwave.internal.protocol.ZWaveAssociationGroup;
 import org.openhab.binding.zwave.internal.protocol.ZWaveConfigurationParameter;
 import org.openhab.binding.zwave.internal.protocol.ZWaveEventListener;
 import org.openhab.binding.zwave.internal.protocol.ZWaveNode;
+import org.openhab.binding.zwave.internal.protocol.ZWaveNodeState;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass.CommandClass;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveConfigurationCommandClass;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveConfigurationCommandClass.ZWaveConfigurationParameterEvent;
@@ -382,8 +383,7 @@ public class ZWaveThingHandler extends ConfigStatusThingHandler implements ZWave
                     if (wakeupCommandClass != null && wakeupCommandClass.getInterval() != 0) {
                         if (node.getLastReceived()
                                 .getTime() < (System.currentTimeMillis() - (wakeupCommandClass.getInterval() * 2000))) {
-                            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-                                    ZWaveBindingConstants.getI18nConstant(ZWaveBindingConstants.OFFLINE_NODE_DEAD));
+                            node.setNodeState(ZWaveNodeState.DEAD);
                         }
                     }
 
