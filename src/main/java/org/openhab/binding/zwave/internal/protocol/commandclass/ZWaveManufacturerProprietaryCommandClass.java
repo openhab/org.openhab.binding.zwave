c430609cbbc7cbff4325d225b5a245cbc76179e4
Chris Jackson <chris@cd-jackson.com>
2017-01-28 10:15:41 +0000

    Initial addition of manufacturer proprietary command class
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ src/main/java/org/openhab/binding/zwave/internal/protocol/commandclass/ZWaveManufacturerProprietaryCommandClass.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.zwave.internal.protocol.commandclass;
+
+import java.util.Map;
+
+import org.openhab.binding.zwave.internal.protocol.ZWaveCommandClassPayload;
+import org.openhab.binding.zwave.internal.protocol.ZWaveController;
+import org.openhab.binding.zwave.internal.protocol.ZWaveEndpoint;
+import org.openhab.binding.zwave.internal.protocol.ZWaveNode;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * ZWave Manufacturer Proprietary command class
+ *
+ * @author Chris Jackson
+ */
+@XStreamAlias("COMMAND_CLASS_MANUFACTURER_PROPRIETARY")
+public class ZWaveManufacturerProprietaryCommandClass extends ZWaveCommandClass {
+    /**
+     * Creates a new instance of the ZWaveManufacturerProprietaryCommandClass class.
+     *
+     * @param node the node this command class belongs to
+     * @param controller the controller to use
+     * @param endpoint the endpoint this Command class belongs to
+     */
+    public ZWaveManufacturerProprietaryCommandClass(ZWaveNode node, ZWaveController controller,
+            ZWaveEndpoint endpoint) {
+        super(node, controller, endpoint);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CommandClass getCommandClass() {
+        return CommandClass.COMMAND_CLASS_MANUFACTURER_PROPRIETARY;
+    }
+
+    @ZWaveResponseHandler(id = 0, name = "DEFAULT_HANDLER")
+    public void handleManufacturerSpecificReport(ZWaveCommandClassPayload payload, int endpoint) {
+
+    }
+
+    @Override
+    public boolean setOptions(Map<String, String> options) {
+        if ("false".equals(options.get("getSupported"))) {
+        }
+
+        return true;
+    }
+}
