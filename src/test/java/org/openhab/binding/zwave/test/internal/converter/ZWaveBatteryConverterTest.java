120454ad1211e7d2c0931d2312b091f3f15d50a6
Chris Jackson <chris@cd-jackson.com>
2017-06-17 17:40:48 +0100

    Allow battery values greater than 100%. Add tests.
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ src/test/java/org/openhab/binding/zwave/test/internal/converter/ZWaveBatteryConverterTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.zwave.test.internal.converter;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.types.State;
+import org.junit.Test;
+import org.openhab.binding.zwave.handler.ZWaveThingChannel;
+import org.openhab.binding.zwave.handler.ZWaveThingChannel.DataType;
+import org.openhab.binding.zwave.internal.converter.ZWaveBatteryConverter;
+import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass.CommandClass;
+import org.openhab.binding.zwave.internal.protocol.event.ZWaveCommandClassValueEvent;
+
+/**
+ *
+ * @author Chris Jackson
+ *
+ */
+public class ZWaveBatteryConverterTest extends ZWaveCommandClassConverterTest {
+    final ChannelUID uid = new ChannelUID("zwave:node:bridge:channel");
+
+    @Test
+    public void Event_Percent() {
+        ZWaveBatteryConverter converter = new ZWaveBatteryConverter(null);
+        ZWaveThingChannel channel = new ZWaveThingChannel(null, uid, DataType.PercentType,
+                CommandClass.COMMAND_CLASS_BATTERY.toString(), 0, new HashMap<String, String>());
+
+        State state;
+        ZWaveCommandClassValueEvent event;
+
+        event = new ZWaveCommandClassValueEvent(1, 0, CommandClass.COMMAND_CLASS_BASIC, 100);
+        state = converter.handleEvent(channel, event);
+        assertEquals(DecimalType.class, state.getClass());
+        assertEquals(state, new DecimalType(100));
+
+        event = new ZWaveCommandClassValueEvent(1, 0, CommandClass.COMMAND_CLASS_BASIC, 50);
+        state = converter.handleEvent(channel, event);
+        assertEquals(DecimalType.class, state.getClass());
+        assertEquals(state, new DecimalType(50));
+
+        event = new ZWaveCommandClassValueEvent(1, 0, CommandClass.COMMAND_CLASS_BASIC, 101);
+        state = converter.handleEvent(channel, event);
+        assertEquals(DecimalType.class, state.getClass());
+        assertEquals(state, new DecimalType(101));
+    }
+}
--- src/test/java/org/openhab/binding/zwave/test/internal/protocol/commandclass/ZWaveBatteryCommandClassTest.java
+++ src/test/java/org/openhab/binding/zwave/test/internal/protocol/commandclass/ZWaveBatteryCommandClassTest.java
@@ -8,13 +8,16 @@
  */
 package org.openhab.binding.zwave.test.internal.protocol.commandclass;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveBatteryCommandClass;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass.CommandClass;
+import org.openhab.binding.zwave.internal.protocol.event.ZWaveCommandClassValueEvent;
+import org.openhab.binding.zwave.internal.protocol.event.ZWaveEvent;
 import org.openhab.binding.zwave.internal.protocol.transaction.ZWaveCommandClassTransactionPayload;
 
 /**
@@ -34,4 +37,21 @@ public class ZWaveBatteryCommandClassTest extends ZWaveCommandClassTest {
         msg = cls.getValueMessage();
         assertTrue(Arrays.equals(msg.getPayloadBuffer(), expectedResponseV1));
     }
+
+    @Test
+    public void handleReport() {
+        byte[] packetData = { 0x01, 0x0A, 0x00, 0x04, 0x00, 0x49, 0x04, 0x71, 0x05, 0x00, 0x00, (byte) 0xC8 };
+
+        List<ZWaveEvent> events = processCommandClassMessage(packetData, 3);
+
+        assertEquals(events.size(), 1);
+
+        ZWaveCommandClassValueEvent event = (ZWaveCommandClassValueEvent) events.get(0);
+
+        // assertEquals(event.getNodeId(), 40);
+        assertEquals(event.getEndpoint(), 0);
+        assertEquals(CommandClass.COMMAND_CLASS_BATTERY, event.getCommandClass());
+        assertEquals(0x00, event.getValue());
+    }
+
 }
