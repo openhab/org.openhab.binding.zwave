4a65e5ba66561522d841ba0f7c976855e548f90c
Chris Jackson <chris@cd-jackson.com>
2018-06-23 22:24:42 +0100

    Database update
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- ESH-INF/thing/dates.sh
+++
@@ -1,7 +0,0 @@
-git ls-tree -r --name-only HEAD | while read filename; do
-    VARA="$(git log -1 --format="%aI" -- "$filename")"
-    VARB="$(awk '/dbReference/ {print $2}' $filename)"
-VARX="${VARB#*>}"
-VARY="${VARX%%<*}"
-echo SET first_approval_time=\""${VARA}"\" WHERE id="${VARY}"
-done
