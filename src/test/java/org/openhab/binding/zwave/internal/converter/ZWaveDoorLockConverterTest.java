9ee6faa7e39d4b64e634aae52c2b0ed3db5cef88
Chris Jackson <chris@cd-jackson.com>
2018-10-16 22:03:08 +0100

    Add door_sensor channel from door lock command class
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ src/test/java/org/openhab/binding/zwave/internal/converter/ZWaveDoorLockConverterTest.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.zwave.internal.converter;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.OpenClosedType;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.type.ChannelTypeUID;
+import org.eclipse.smarthome.core.types.State;
+import org.junit.Test;
+import org.openhab.binding.zwave.handler.ZWaveThingChannel;
+import org.openhab.binding.zwave.handler.ZWaveThingChannel.DataType;
+import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass.CommandClass;
+import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveDoorLockCommandClass;
+import org.openhab.binding.zwave.internal.protocol.event.ZWaveCommandClassValueEvent;
+
+/**
+ *
+ * @author Chris Jackson
+ *
+ */
+public class ZWaveDoorLockConverterTest extends ZWaveCommandClassConverterTest {
+    final ChannelUID uidLockDoor = new ChannelUID("zwave:node:bridge:lock_door");
+    final ChannelTypeUID typeUidLockDoor = new ChannelTypeUID("zwave:lock_door");
+    final ChannelUID uidSensorDoor = new ChannelUID("zwave:node:bridge:sensor_door");
+    final ChannelTypeUID typeUidSensorDoor = new ChannelTypeUID("zwave:sensor_door");
+
+    @Test
+    public void Event_DoorLockState() {
+        ZWaveDoorLockConverter converter = new ZWaveDoorLockConverter(null);
+        ZWaveThingChannel channel = new ZWaveThingChannel(null, typeUidLockDoor, uidLockDoor, DataType.OnOffType,
+                CommandClass.COMMAND_CLASS_DOOR_LOCK.toString(), 0, new HashMap<String, String>());
+
+        State state;
+        ZWaveCommandClassValueEvent event;
+
+        event = new ZWaveCommandClassValueEvent(1, 0, CommandClass.COMMAND_CLASS_DOOR_LOCK, 0,
+                ZWaveDoorLockCommandClass.Type.DOOR_LOCK_STATE);
+        state = converter.handleEvent(channel, event);
+        assertEquals(OnOffType.class, state.getClass());
+        assertEquals(state, OnOffType.OFF);
+
+        event = new ZWaveCommandClassValueEvent(1, 0, CommandClass.COMMAND_CLASS_DOOR_LOCK, 1,
+                ZWaveDoorLockCommandClass.Type.DOOR_LOCK_STATE);
+        state = converter.handleEvent(channel, event);
+        assertEquals(OnOffType.class, state.getClass());
+        assertEquals(state, OnOffType.ON);
+    }
+
+    @Test
+    public void Event_DoorLockCondition() {
+        ZWaveDoorLockConverter converter = new ZWaveDoorLockConverter(null);
+        ZWaveThingChannel channel = new ZWaveThingChannel(null, typeUidSensorDoor, uidSensorDoor,
+                DataType.OpenClosedType, CommandClass.COMMAND_CLASS_DOOR_LOCK.toString(), 0,
+                new HashMap<String, String>());
+
+        State state;
+        ZWaveCommandClassValueEvent event;
+
+        event = new ZWaveCommandClassValueEvent(1, 0, CommandClass.COMMAND_CLASS_DOOR_LOCK, 0,
+                ZWaveDoorLockCommandClass.Type.DOOR_CONDITION);
+        state = converter.handleEvent(channel, event);
+        assertEquals(OpenClosedType.class, state.getClass());
+        assertEquals(state, OpenClosedType.OPEN);
+
+        event = new ZWaveCommandClassValueEvent(1, 0, CommandClass.COMMAND_CLASS_DOOR_LOCK, 1,
+                ZWaveDoorLockCommandClass.Type.DOOR_CONDITION);
+        state = converter.handleEvent(channel, event);
+        assertEquals(OpenClosedType.class, state.getClass());
+        assertEquals(state, OpenClosedType.CLOSED);
+    }
+}
